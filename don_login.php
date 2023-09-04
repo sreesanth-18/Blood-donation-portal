7ee1a52a3ca36abd72ed96e12ec6b72a3469e920
sreesanth-18 <sreesanthciby@gmail.com>
2023-09-04 09:32:50 +0530

    action page for donor login

---
+++ don_login.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        $usrname = $_POST['usrname'];
+        $pwd = md5($_POST['pwd']);
+        include 'conn.php';
+        $sql = "SELECT * FROM donorreg WHERE usrname='$usrname' AND pwd='$pwd'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 1) {
+            $_SESSION['username'] = $usrname;
+            header("Location: patient_form.html");
+            exit();
+        } 
+        else 
+        {
+            echo "Invalid username or password. Please try again.";
+        }
+        $conn->close();
+    }
+?>
