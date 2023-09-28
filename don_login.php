f0de4036baed39b6fff937701688909e8c74be7a
sreesanth-18 <sreesanthciby@gmail.com>
2023-09-28 21:39:21 +0530

    adjusted vars to match the new password encryption algorithm

--- don_login.php
+++ don_login.php
@@ -4,19 +4,36 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
         $usrname = $_POST['usrname'];
-        $pwd = md5($_POST['pwd']);
+        $password = $_POST['pwd'];
         include 'conn.php';
-        $sql = "SELECT * FROM donorreg WHERE usrname='$usrname' AND pwd='$pwd'";
+
+        $sql = "SELECT pwd FROM donorreg WHERE usrname='$usrname'";
         $result = $conn->query($sql);
 
-        if ($result->num_rows == 1) {
-            $_SESSION['username'] = $usrname;
-            header("Location: patient_form.html");
-            exit();
+        if ($result->num_rows == 1) 
+        {
+            $row = $result->fetch_assoc();
+            $hashedPasswordFromDB = $row['pwd'];
+
+            // Verify the entered password against the stored hashed password
+            if (password_verify($password, $hashedPasswordFromDB)) 
+            {
+                $_SESSION['username'] = $usrname;
+                header("Location: dashboard.html");
+                exit();
+            }
+            else
+            {
+                echo '<script>alert("Inncorrect password. Please try again");
+                window.location.href = "don_login.html";
+                </script>'; 
+            }
         } 
         else 
         {
-            echo "Invalid username or password. Please try again.";
+            echo '<script>alert("Username does not exist. Please register");
+            window.location.href = "donor_form.html";
+            </script>';
         }
         $conn->close();
     }
--- pat_login.php
+++ pat_login.php
@@ -1,22 +1,40 @@
 <?php
     session_start();
-    
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
         $usrname = $_POST['usrname'];
-        $pwd = md5($_POST['pwd']);
+        $password = $_POST['pwd']; // Password entered by the user
         include 'conn.php';
-        $sql = "SELECT * FROM patreg WHERE usrname='$usrname' AND pwd='$pwd'";
+        
+        // Fetch the hashed password from the database based on the username
+        $sql = "SELECT pwd FROM patreg WHERE usrname='$usrname'";
         $result = $conn->query($sql);
 
-        if ($result->num_rows == 1) {
-            $_SESSION['username'] = $usrname;
-            header("Location: patient_form.html");
-            exit();
-        } 
+        if ($result->num_rows == 1) 
+        {
+            $row = $result->fetch_assoc();
+            $hashedPasswordFromDB = $row['pwd'];
+
+            // Verify the entered password against the stored hashed password
+            if (password_verify($password, $hashedPasswordFromDB)) 
+            {
+                $_SESSION['username'] = $usrname;
+                header("Location: dashboard.html");
+                exit();
+            }
+            else
+            {
+                echo '<script>alert("Inncorrect password. Please try again");
+                window.location.href = "pat_login.html";
+                </script>'; 
+            }
+        }
         else 
         {
-            echo "Invalid username or password. Please try again.";
+            echo '<script>alert("Username does not exist. Please register");
+            window.location.href = "patient_form.html";
+            </script>';
         }
         $conn->close();
     }
