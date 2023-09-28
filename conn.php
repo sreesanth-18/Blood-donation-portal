61fcd97734f89f8d7852eb6cd5e3eca4670fef92
sreesanth-18 <sreesanthciby@gmail.com>
2023-09-28 21:33:36 +0530

    reusable mysql connectivity:

--- conn.php
+++ conn.php
@@ -1,9 +1,7 @@
-//Establish connection to the MySQL database 'bloodbank'
 <?php
     $conn = new mysqli("localhost","root", "","bloodbank");
     if ($conn->connect_error) 
     {
         die("Connection failed: " . $conn->connect_error);
     }
-
 ?>
