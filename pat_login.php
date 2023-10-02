63791d6e18c45d5351cd60c270f0bcb275d313fa
sreesanth-18 <sreesanthciby@gmail.com>
2023-10-02 19:57:45 +0530

    set session variables

--- pat_login.php
+++ pat_login.php
@@ -1,41 +1,37 @@
 <?php
     session_start();
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") 
-    {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $usrname = $_POST['usrname'];
         $password = $_POST['pwd']; // Password entered by the user
         include 'conn.php';
-        
-        // Fetch the hashed password from the database based on the username
-        $sql = "SELECT pwd FROM patreg WHERE patusr='$usrname'";
+    
+        // Fetch the hashed password and bloodgroup from the database based on the username
+        $sql = "SELECT pwd, bgroup FROM patreg WHERE patusr='$usrname'";
         $result = $conn->query($sql);
-
-        if ($result->num_rows == 1) 
-        {
+    
+        if ($result->num_rows == 1) {
             $row = $result->fetch_assoc();
             $hashedPasswordFromDB = $row['pwd'];
-
+            $bloodgroup = $row['bgroup']; // Retrieve bloodgroup
+    
             // Verify the entered password against the stored hashed password
-            if (password_verify($password, $hashedPasswordFromDB)) 
-            {
-                $_SESSION['usrname'] = $usrname;
+            if (password_verify($password, $hashedPasswordFromDB)) {
+                $_SESSION['patusr'] = $usrname;
+                $_SESSION['bgroup'] = $bloodgroup; // Store bloodgroup in session
                 header("Location: pat_dashboard.php");
                 exit();
-            }
-            else
-            {
-                echo '<script>alert("Inncorrect password. Please try again");
+            } else {
+                echo '<script>alert("Incorrect password. Please try again");
                 window.location.href = "pat_login.html";
-                </script>'; 
+                </script>';
             }
-        }
-        else 
-        {
+        } else {
             echo '<script>alert("Username does not exist. Please register");
             window.location.href = "patient_form.html";
             </script>';
         }
         $conn->close();
     }
+    
 ?>
