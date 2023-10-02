c248fffb03fc8599899af9c71c2aade5ec38b856
sreesanth-18 <sreesanthciby@gmail.com>
2023-10-02 23:52:13 +0530

    header for successfull request

---
+++ request_success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Successful</title>
+    <link rel="stylesheet" href="css/req.css">
+    <!-- Add your CSS stylesheets and fonts here -->
+</head>
+<body>
+    <?php
+            include_once 'header.php';
+    ?>
+    <div class="background"></div>
+    <section class="container">
+        <h1>Request sent successfully</h1>
+        <p>Your request was successfully sent. Kindly check your <a href="account_dashboard.php">Account Dashboard</a> to know the status or, return to the <a href="pat_dashboard.php">Home page</a>.</p> <!-- Link to the home page -->
+    </section>
+</body>
+</html>
